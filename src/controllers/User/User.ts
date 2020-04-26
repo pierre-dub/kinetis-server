0e9eacfa7d898aedc9b959a18590d45d65a9a9d8
Pierre <pierre74d@gmail.com>
2020-04-26 16:57:23 +0200

    New POST to get precise workout

--- src/controllers/User/User.ts
+++ src/controllers/User/User.ts
@@ -1,10 +1,12 @@
 
 import { Request, Response } from 'express';
 import { CrudController } from '../CrudController';
+import {getWorkoutWithTitle} from "../../db/Workout/getWorkout";
 
 export class UserController extends CrudController {
     public create(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-        throw new Error("Method not implemented.");
+        console.log("/GET Titled Workout");
+        getWorkoutWithTitle(req.body.title).then((response) => {res.json(response)})
     }
 
     public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
--- src/db/Workout/getWorkout.ts
+++ src/db/Workout/getWorkout.ts
@@ -19,3 +19,24 @@ export async function getWorkout() {
         })
     })
 }
+
+export async function getWorkoutWithTitle(titleSearched:string) {
+    let response: any;
+    const param = '%'+titleSearched+'%'
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM WORKOUT WHERE TITLE LIKE $title",{$title:param}, async function (err: any, res: any) {
+            if (err) {
+                reject(err);
+                console.error(err);
+            }
+            else {
+                response = {
+                    status: "success",
+                    data: res,
+                    message: "GET success"
+                }
+                resolve(response)
+            }
+        })
+    })
+}
