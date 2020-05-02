0c819afc89c0e2faa03c6c49000dc69613dbd14f
Pierre <pierre74d@gmail.com>
2020-05-02 14:23:12 +0200

    Return id when new workout is created

--- src/db/Workout/addWorkout.ts
+++ src/db/Workout/addWorkout.ts
@@ -5,7 +5,7 @@ export function addWorkout(id:number, title: string, description: string, materi
     return new Promise((resolve, reject) => {
         db.run("INSERT INTO WORKOUT VALUES ($id, $title, $description, $materiel, $repetition, $obj)",
             {$id:id, $title:title ,$description:description, $materiel:materiel, $repetition:repetition, $obj:objectif},
-            function (err:any) {
+            async function (err:any, res:any) {
                 if (err) {
                     console.error(err)
                     reject(err);
@@ -13,7 +13,7 @@ export function addWorkout(id:number, title: string, description: string, materi
                 else {
                     response = {
                         status: "success",
-                        data: null,
+                        data: `${this.lastID}`,
                         message: "POST success" /* Or optional success message */
                     }
                 }
