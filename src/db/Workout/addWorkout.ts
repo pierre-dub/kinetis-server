76a80155d2c1287ea7a4b8d1923fa56e3df565d9
Pierre <pierre74d@gmail.com>
2020-05-09 12:33:22 +0200

    Handle case when no image is uploaded

--- src/db/Workout/addWorkout.ts
+++ src/db/Workout/addWorkout.ts
@@ -2,9 +2,16 @@ import {db} from "../../index";
 
 export function addWorkout(id:number, title: string, description: string, materiel: string, repetition: string, objectif: string, image :any) {
     let response:any
+    let dataImage:any
+    if(image === undefined){
+        dataImage = null
+    }
+    else{
+        dataImage = image.base64
+    }
     return new Promise((resolve, reject) => {
         db.run("INSERT INTO WORKOUT VALUES ($id, $title, $description, $materiel, $repetition, $obj, $image)",
-            {$id:id, $title:title ,$description:description, $materiel:materiel, $repetition:repetition, $obj:objectif, $image:image.base64},
+            {$id:id, $title:title ,$description:description, $materiel:materiel, $repetition:repetition, $obj:objectif, $image:dataImage},
             async function (err:any, res:any) {
                 if (err) {
                     console.error(err)
