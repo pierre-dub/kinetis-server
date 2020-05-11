2e389217738f7626ba0dc4682a2feead232a9600
Pierre <pierre74d@gmail.com>
2020-05-11 13:58:28 +0200

    Remove unused log

--- src/db/Workout/updateWorkout.ts
+++ src/db/Workout/updateWorkout.ts
@@ -2,7 +2,6 @@ import {db} from "../../index";
 
 export function updateWorkout(id:number, title: string, description: string, materiel: string, repetition: string, objectif: string) {
     let response:any
-    console.log(title)
     return new Promise((resolve, reject) => {
         db.run("UPDATE WORKOUT SET TITLE=$title, DESCRIPTION=$description, MATERIEL=$materiel, REPETITION=$repetition, OBJ=$objectif WHERE ID=$id",
             {$id:id, $title:title ,$description:description, $materiel:materiel, $repetition:repetition, $objectif:objectif,},
